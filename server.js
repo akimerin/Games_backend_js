a9ce7d53a7d7aca12acb2854b513342cfc5f0507
Anastasia <v4silieva.an45@yandex.ru>
2025-06-16 14:05:58 +0300

    UPDATE SERVER PART

--- server.js
+++ server.js
@@ -1,48 +1,68 @@
+// server.js
 const express = require('express');
-const {MongoClient,ObjectId} = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb');
+
 const app = express();
 const port = 8001;
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');           
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);  
+  next();
+});
+
 app.use(express.json());
 app.use(express.static('public'));
 
-async function getDbCollection(dbAdress, dbName, dbCollectionName){
-	const client = new MongoClient (dbAdress);
-	await client.connect();
-	const db = client.db(dbName);
-	return db.collection(dbCollectionName);
+async function getDbCollection(dbAdress, dbName, dbCollectionName) {
+  const client = new MongoClient(dbAdress);
+  await client.connect();
+  const db = client.db(dbName);
+  return db.collection(dbCollectionName);
 }
 
-app.get('/tasks', async function (req, res){
-	const collection = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
-	const data = await collection.find({}).toArray();
-	res.send(data);
+app.get('/tasks', async (req, res) => {
+  const col = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
+  const data = await col.find({}).toArray();
+  res.json(data);
 });
 
-app.get('/tasks/:id', async function (req, res){
-	const collection = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
-	const data = await collection.findOne({_id: new ObjectId(req.params.id)});
-	res.send(data);
+app.get('/tasks/:id', async (req, res) => {
+  const col = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
+  const item = await col.findOne({ _id: new ObjectId(req.params.id) });
+  if (!item) return res.status(404).json({ error: 'Not found' });
+  res.json(item);
 });
 
-app.post('/tasks', async function (req, res){
-	const task = {...req.body, done: false};
-	const collection = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
-	await collection.insertOne(task);
-	res.send(task);
+app.post('/tasks', async (req, res) => {
+  const task = { ...req.body, done: false };
+  const col = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
+  await col.insertOne(task);
+  res.json(task);
 });
 
-app.patch('/tasks/:id', async function (req, res){
-	const collection = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
-	const data = await collection.updateOne({_id: new ObjectId(req.params.id)}, {'$set': req.body});
-	res.send({});
+app.patch('/tasks/:id', async (req, res) => {
+  const { _id, ...toUpdate } = req.body;
+  const col = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
+  const result = await col.updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: toUpdate }
+  );
+  if (result.matchedCount === 0) return res.status(404).json({ error: 'Not found' });
+  const updated = await col.findOne({ _id: new ObjectId(req.params.id) });
+  res.json(updated);
 });
 
-app.delete('/tasks/:id', async function (req, res){
-	const collection = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
-	await collection.deleteOne({_id: new ObjectId(req.params.id)});
-	res.send({});
+app.delete('/tasks/:id', async (req, res) => {
+  const col = await getDbCollection('mongodb://localhost', 'todoapp', 'tasks');
+  const result = await col.deleteOne({ _id: new ObjectId(req.params.id) });
+  if (result.deletedCount === 0) return res.status(404).json({ error: 'Not found' });
+  res.json({ success: true });
 });
 
-app.listen(port, function(){
-	console.log('server is started');
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
